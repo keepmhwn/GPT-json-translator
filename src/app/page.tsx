eeb150ea04f8ae1764118a41de738a02a51bd3d8
kimmeh <mhko0411@gmail.com>
2024-09-14 14:07:42 +0900

    feat: add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,13 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-
-import request from "@/api/request";
-
-import { Editor, type OnMount, OnChange } from "@monaco-editor/react";
-import * as monacoEditor from "monaco-editor";
+import { Stack, Text } from "@chakra-ui/react";
 
 export default function Home() {
-  const editorRef = useRef<monacoEditor.editor.IStandaloneCodeEditor | null>(
-    null
-  );
-
-  const handleEditorDidMount: OnMount = (editor) => {
-    editorRef.current = editor;
-  };
-
-  const handleChangeCode: OnChange = (value: string | undefined) => {
-    console.log(value);
-  };
-
-  useEffect(() => {
-    const translate = async () => {
-      const response = await request({
-        path: "/api/translate",
-        method: "POST",
-        body: {
-          text: "사과",
-          targetLanguage: "ko",
-        },
-      });
-
-      console.log(response);
-    };
-
-    translate().then();
-  }, []);
-
   return (
-    <div>
-      <Editor
-        defaultLanguage="JSON"
-        onMount={handleEditorDidMount}
-        onChange={handleChangeCode}
-      />
-    </div>
+    <Stack spacing={4} alignItems="center">
+      <Text as="b" fontSize="2xl" color="green">
+        i18next JSON Translator
+      </Text>
+    </Stack>
   );
 }
