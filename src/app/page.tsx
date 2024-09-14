b075c3e72e678dc4bef8862379b2320b2747173a
kimmeh <mhko0411@gmail.com>
2024-09-14 22:56:01 +0900

    feat: add json editor form component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-"use client";
-
 import { Stack, Text } from "@chakra-ui/react";
+
+import I18NextJsonForm from "./sections/i18next-json-form";
 
 export default function Home() {
   return (
@@ -8,6 +8,7 @@ export default function Home() {
       <Text as="b" fontSize="2xl" color="green">
         i18next JSON Translator
       </Text>
+      <I18NextJsonForm />
     </Stack>
   );
 }
