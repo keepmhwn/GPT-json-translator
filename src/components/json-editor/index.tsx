914e702410fa2cdfef1b81ef3aa34d4cc97a2b01
kimmeh <mhko0411@gmail.com>
2024-09-14 14:07:56 +0900

    feat: add json editor

---
+++ src/components/json-editor/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useRef } from "react";
+
+import { Box, Spinner } from "@chakra-ui/react";
+
+import {
+  Editor,
+  type OnMount,
+  type OnChange,
+  type OnValidate,
+} from "@monaco-editor/react";
+import * as monacoEditor from "monaco-editor";
+
+type Props = {
+  value: string;
+  onChange: OnChange;
+  onValidate: (isValid: boolean) => void;
+};
+
+const JsonEditor = ({ value, onChange, onValidate }: Props) => {
+  const editorRef = useRef<monacoEditor.editor.IStandaloneCodeEditor | null>(
+    null
+  );
+
+  const handleEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  const handleValidate: OnValidate = (markers) => {
+    const isValid = markers.length === 0;
+    onValidate(isValid);
+  };
+
+  return (
+    <Box padding="16px" borderWidth="1px" borderRadius="lg">
+      <Editor
+        width="100%"
+        height="100%"
+        language="json"
+        value={value}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
+        loading={<Spinner size="xl" />}
+        onMount={handleEditorDidMount}
+        onValidate={handleValidate}
+        onChange={onChange}
+      />
+    </Box>
+  );
+};
+
+export default JsonEditor;
