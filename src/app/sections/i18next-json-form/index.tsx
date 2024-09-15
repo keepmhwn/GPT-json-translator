5aa2f7e31d561664619ded49051f92b3220731b0
kimmeh <mhko0411@gmail.com>
2024-09-15 10:51:31 +0900

    feat: connect translate api

--- src/app/sections/i18next-json-form/index.tsx
+++ src/app/sections/i18next-json-form/index.tsx
@@ -3,10 +3,27 @@ import { useState } from "react";
 
 import I18nextJsonFormLogic from "./logic";
 
+import request from "@/api/request";
+
 const I18NextJsonForm = () => {
   const [translated, setTranslated] = useState("");
 
-  const handleTranslate = (sourceJson: string) => {};
+  const handleTranslate = async (sourceJson: string) => {
+    try {
+      const { translated } = await request({
+        path: "/api/translate",
+        method: "POST",
+        body: {
+          sourceJson,
+          targetLanguage: "Japanese",
+        },
+      });
+
+      setTranslated(translated.content);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <I18nextJsonFormLogic
