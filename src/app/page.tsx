81dc2068d5ed0106d815cdb972073c660869764b
kimmeh <mhko0411@gmail.com>
2024-09-17 13:25:40 +0900

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,21 @@ import I18NextJsonForm from "./sections/i18next-json-form";
 
 export default function Home() {
   return (
-    <Stack spacing={4} alignItems="center">
-      <Text as="b" fontSize="2xl" color="green">
-        i18next JSON Translator
-      </Text>
-      <I18NextJsonForm />
-    </Stack>
+    <>
+      <Stack spacing={4} alignItems="center" padding={8}>
+        <Text as="b" fontSize="2xl" color="green">
+          i18next JSON Translator
+        </Text>
+        <I18NextJsonForm />
+        <footer>
+          <Text as="i" fontSize="sm" color="blackAlpha.500">
+            Please contact us to add a language to support.
+          </Text>
+          <Text fontSize="sm" textAlign="center" color="blackAlpha.500">
+            mhko0411@gmail.com
+          </Text>
+        </footer>
+      </Stack>
+    </>
   );
 }
